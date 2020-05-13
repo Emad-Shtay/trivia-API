6150ff3bc3ab4a1c6c63bb29af1bc9153b8713d8
Emad-Shtay <emad.shtay@yandex.ru>
2020-05-14 01:43:07 +0300

    Enable Cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -29,6 +29,7 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
+            \Barryvdh\Cors\HandleCors::class,
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
@@ -41,6 +42,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Barryvdh\Cors\HandleCors::class,
         ],
     ];
 
